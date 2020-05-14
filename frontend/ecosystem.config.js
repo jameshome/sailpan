62cf9afa4d359bdc9ae9d7b8b60750d90b9799fb
James Home <jh@jameshome.com>
2020-05-14 14:26:57 -0500

    prepare for live stat update

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       cwd: "/domains/sailpan.info/dist/frontend",
       script: "yarn",
       args: "start",
+      watch: true,
       interpreter: "/bin/bash",
       env: {
         NODE_ENV: "production",
